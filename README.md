b44fe900960f8fc3559017d3eb45ead8ace24418
Mamodesaebsaad <saadmamodesaeb@gmail.com>
2022-10-14 04:18:32 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### NATIONAL IDENTITY NUMBER VALIDATOR
+This Module validate the NIN of a user by doing a mathematical operation.
+
+The Operation is as follows:
+
+![](national-identity-calculation.png)
---
+++ lib/cjs/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * The function takes a string as an argument and returns a boolean value
+ * @param {string} n - string - The national identity number.
+ * @returns A boolean value.
+ */
+declare const validateNationalIdentityNumber: (n: string) => boolean;
+export default validateNationalIdentityNumber;
